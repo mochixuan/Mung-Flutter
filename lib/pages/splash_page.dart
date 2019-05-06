5d8faacc6e530a7dff407e2ed6a799953e2cc7c8
wang <1306894924@qq.com>
2019-05-06 18:54:09 +0800

    config splash

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new Text("测试"),
+    );
+  }
+}
