e8fadfa369d6014cae9b075cfcfe44785cf60af2
wang <1306894924@qq.com>
2019-05-14 08:57:47 +0800

    完成iOS splash config

--- lib/pages/theme_page.dart
+++ lib/pages/theme_page.dart
@@ -12,60 +12,61 @@ class ThemePage extends StatelessWidget {
     const _gridSpacing = 8.0;
 
     return Scaffold(
+
       appBar: AppBar(
         leading: UiUtil.getIconFontButton(0xeb09,() => Navigator.pop(context)),
         title: Text("主题",style: TextStyle(color: WColors.white)),
-        centerTitle: true
+        centerTitle: true,
       ),
       body: Container(
         padding: const EdgeInsets.all(_gridSpacing),
         child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            childAspectRatio: 5/6,
-            crossAxisSpacing: _gridSpacing,
-            mainAxisSpacing: _gridSpacing,
-            crossAxisCount: 3,
-          ),
-          itemCount: Constant.ThemeItems.length,
-          itemBuilder: (context,index) {
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              childAspectRatio: 5/6,
+              crossAxisSpacing: _gridSpacing,
+              mainAxisSpacing: _gridSpacing,
+              crossAxisCount: 3,
+            ),
+            itemCount: Constant.ThemeItems.length,
+            itemBuilder: (context,index) {
 
-            final _themeModel = Constant.ThemeItems[index];
+              final _themeModel = Constant.ThemeItems[index];
 
-            return FlatButton(
-              padding: EdgeInsets.all(0),
-              onPressed: () {
-                ThemeProvider.of(context).changeTheme(_themeModel['color']);
-              },
-              child: Column(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Color(_themeModel['color']),
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.elliptical(4, 4),
-                              topRight: Radius.elliptical(4, 4)
-                          )
+              return FlatButton(
+                padding: EdgeInsets.all(0),
+                onPressed: () {
+                  ThemeProvider.of(context).changeTheme(_themeModel['color']);
+                },
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Color(_themeModel['color']),
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.elliptical(4, 4),
+                                topRight: Radius.elliptical(4, 4)
+                            )
+                        ),
                       ),
                     ),
-                  ),
-                  Container(
-                    height: 40,
-                    alignment: Alignment.center,
-                    color: WColors.white,
-                    child: Text(
-                      _themeModel['name'],
-                      style: TextStyle(
-                          fontSize: 14.0,
-                          color: Color(_themeModel['color']),
-                          fontWeight: FontWeight.bold
+                    Container(
+                      height: 40,
+                      alignment: Alignment.center,
+                      color: WColors.white,
+                      child: Text(
+                        _themeModel['name'],
+                        style: TextStyle(
+                            fontSize: 14.0,
+                            color: Color(_themeModel['color']),
+                            fontWeight: FontWeight.bold
+                        ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-            );
-          }
+                    )
+                  ],
+                ),
+              );
+            }
         ),
       ),
     );
